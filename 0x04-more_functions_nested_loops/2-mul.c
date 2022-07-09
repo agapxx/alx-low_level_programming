5785f9bf3210c8c5cf16559d0196648afa888e0c
agapxx <okontaagape@gmail.com>
2022-07-08 17:08:26 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include "holberton.h"
->>>>>>> d26f8fc637d417d198c4c35319b177a6d4824bf9
 
 /**
  * mul -  checks for checks for a digit (0 through 9).
@@ -19,10 +15,4 @@ int mul(int a, int b)
 
 	return (mul);
 }
-<<<<<<< HEAD
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
 
-=======
->>>>>>> d26f8fc637d417d198c4c35319b177a6d4824bf9
