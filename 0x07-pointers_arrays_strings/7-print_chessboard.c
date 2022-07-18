4aeffc577446c9dc821c1b6e651d984e65c08ce3
agapxx <okontaagape@gmail.com>
2022-07-18 14:34:55 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints buffer in hexa
- * @a: cheese
- * needle: buffer2
- *
- * Return: Nothing.
- */
+* print_chessboard - prints buffer in hexa
+* @a: cheese
+* needle: buffer2
+*
+* Return: Nothing.
+*/
+
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar (a[i][j]);
-		}
-	_putchar('\n');
-		}
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+putchar (a[i][j]);
+}
+putchar('\n');
+}
 }
