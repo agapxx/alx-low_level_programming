27a1c0f855e752314a8860097419fd402b6a05de
agapxx <okontaagape@gmail.com>
2022-07-20 15:54:57 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _print_rev_recursion - check the code for Alx School students.
+* @s: character of a string
+* Return: Always 0.
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
+}
