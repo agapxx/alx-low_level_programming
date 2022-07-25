f329eb18935da5712491de6a2f2c95fb70b251d5
agapxx <okontaagape@gmail.com>
2022-07-25 16:53:07 -0700

    Task 5

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,7 @@ void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
