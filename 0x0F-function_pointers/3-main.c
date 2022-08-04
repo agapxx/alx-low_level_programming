a2523621dfb36f674bae6dba17a1622576add969
agapxx <okontaagape@gmail.com>
2022-08-03 17:50:59 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,51 +1,46 @@
 #include <stdio.h>
-#include "function_pointers.h"
-#include <stdlib.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * main - check the program
- * @argc: arguments cant
- * @argv: vector arguments
- * Return: 0 if false, something else otherwise.
- */
-int main(int argc, char *argv[])
+ * main - main
+ * @argc: arg count
+ * @argv: arg array
+ *
+ * Return: int
+ *
+ **/
+int main(int argc, char **argv)
 {
-	int (*get)(int, int);
-	int a, b, operator;
-
+	int inta, intb;
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	if (strlen(argv[2]) != 1)
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
-	operator = argv[2][0];
-	switch (operator)
+
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
+	      argv[2][0] != '*' && argv[2][0] != '%') || strlen(argv[2]) != 1)
 	{
-	case '+':
-		break;
-	case '-':
-		break;
-	case '*':
-		break;
-	case '/':
-		break;
-	case '%':
-		break;
-	default:
 		printf("Error\n");
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	get = get_op_func(argv[2]);
-	printf("%d\n", get(a, b));
-	return (0);
+		inta = atoi(argv[1]);
+		intb = atoi(argv[3]);
+
+		printf("%d\n", f(inta, intb));
+
+		return (0);
+
 }
